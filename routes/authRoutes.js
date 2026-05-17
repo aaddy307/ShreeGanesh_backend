b78a9d0d612f4647e0fdb7d8c8fefbcec52fe783
Aaddy <aaddy307@gmail.com>
2026-05-17 13:45:15 +0530

    feat: add POST /create-admin route to register administrative users

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,6 +5,20 @@ const { protect } = require('../middleware/authMiddleware');
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+router.post('/create-admin', async (req, res) => {
+  const { name, email, password } = req.body;
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    return res.status(400).json({ message: 'User already exists' });
+  }
+  const user = await User.create({
+    name,
+    email,
+    password,
+    isAdmin: true,
+  });
+  res.json({ message: 'Admin created', email: user.email });
+});
 router.get('/profile', protect, getUserProfile);
 
 module.exports = router;
